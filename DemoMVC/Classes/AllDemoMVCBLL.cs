f0f5bbafcf8d4dd759b362eb1e2452e80541755f
hiteshcemba <hitesh.cemba@outlook.com>
2015-11-19 22:14:01 -0500

    test 2

--- DemoMVC/Classes/AllDemoMVCBLL.cs
+++ DemoMVC/Classes/AllDemoMVCBLL.cs
@@ -2,26 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DemoMVC.Models;
 
 namespace DemoMVC.Classes
 {
     public class AllDemoMVCBLL
     {
-        public AllDemoMVCBLL(string strconnectionstring)
+        //private DemoMVCDB _DB = null;
+
+        //public DemoMVCDB DB { get { return _DB; } }
+
+        //public List<Category> GetAllCategories()
+        //{
+        //    return DB.GetAllCategories(); 
+        //}
+        //public bool AddCategory(Category objCat)
+        //{
+        //    return DB.AddCategory(objCat); 
+        //}
+        //public Category  FindCategoryWithID(int id)
+
+        //{
+        //    return DB.FindCategoryWithID(id); 
+        //}
+        //public bool EditCategory(Category objCat)
+        //{
+        //    return DB.EditCategory(objCat); 
+        //}
+        //public bool DeleteCategory(Category objCat)
+        //{
+        //    return DB.DeleteCategory(objCat); 
+        //}
+
+
+        public void Dispose()
+        {
+            //this.Dispose();
+        }
+
+
+        public AllDemoMVCBLL()
         {
             Categories = new Categories();
             Logins = new Logins();
             SubCategories = new SubCategories();
+            Receipies = new Receipies();
         }
-        public void Dispose()
-        {
-
-        }
-
-
         public Categories Categories { get; set; }
         public Logins Logins { get; set; }
         public SubCategories SubCategories { get; set; }
+        public Receipies Receipies { get; set; }
+
+
 
     }
 }
